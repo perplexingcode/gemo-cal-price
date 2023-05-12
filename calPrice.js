d058501d49f030079947b9f17924fa718814d740
perplexingcode <perplexingcode@gmail.com>
2023-05-12 22:12:42 +0700

    function 1 + update concept.md

---
+++ calPrice.js
@@ -0,0 +1,45 @@
+function calculatePrice1(drinkType, size, whippedCream) {
+  //  Options
+  const drinkTypes = ['hot', 'cold', 'blended'];
+  const sizes = ['S', 'M', 'L'];
+  const creams = ['withCream', 'withoutCream'];
+
+  const sizeMarkups = {
+    S: 0,
+    M: 0.5,
+    L: 1,
+  };
+
+  const drinkTypeMarkups = {
+    hot: 0,
+    cold: 0,
+    blended: 1,
+  };
+
+  const creamMarkups = {
+    withCream: 0.5,
+    withoutCream: 0,
+  };
+
+  // Exceptions
+  if (size === 'L' && drinkType === 'hot') {
+    alert('Large size is only available for cold and blended drinks.');
+  }
+
+  if (
+    drinkTypes.indexOf(drinkType) === -1 ||
+    sizes.indexOf(size) === -1 ||
+    creams.indexOf(whippedCream) === -1
+  ) {
+    alert('Invalid input, please double check your order.');
+  }
+
+  // Price calculation
+  let price = 2; // base price
+
+  price += sizeMarkups[size];
+  price += creamMarkups[whippedCream];
+  price += drinkTypeMarkups[drinkType];
+
+  return price;
+}
---
+++ concept.md
@@ -0,0 +1,3 @@
+- Because the usesr are likely to be some youngsters operating the cafe, I'll use friendly natural language for input options.
+
+- I will set the options in objects so they can be flexibly changed later.
